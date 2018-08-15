cd4b2cc0638a44bdcba407cec47b4a78e8bf9289
jenriellegaon <jenriellegaon>
2018-08-15 22:21:25 +0800

    Add details view contract

--- app/src/main/java/com/jproject/ytsmoviebrowser/contract/DetailsContract.java
+++ app/src/main/java/com/jproject/ytsmoviebrowser/contract/DetailsContract.java
@@ -6,17 +6,13 @@ public interface DetailsContract {
 
     interface View {
         void initViews();
-
         void showToast(String message);
-
         void showError(String error);
-
         void showMovieDetails(ResObj resObj);
     }
 
     interface CardClickListener {
         void onClick(android.view.View view, int position);
-
         void onLongClick(android.view.View view, int position);
     }
 
