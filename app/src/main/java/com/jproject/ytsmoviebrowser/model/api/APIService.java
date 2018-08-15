ea1a28e5bbe2dc49d32da44e07b847fa81b16a78
jenriellegaon <jenriellegaon>
2018-08-15 22:27:59 +0800

    Update APIService

--- app/src/main/java/com/jproject/ytsmoviebrowser/model/api/APIService.java
+++ app/src/main/java/com/jproject/ytsmoviebrowser/model/api/APIService.java
@@ -9,18 +9,27 @@ import retrofit2.http.Query;
 public interface APIService {
 
     @GET(Client.DEFAULT_LIMITED)
-    Observable<ResObj> getTopDownloads(@Query(value = "sort_by") String topDownloads);
+    Observable<ResObj>
+    getTopDownloads(@Query(value = "sort_by") String topDownloads);
 
     @GET(Client.DEFAULT_LIMITED)
-    Observable<ResObj> getTopRated(@Query(value = "sort_by") String topRated);
+    Observable<ResObj>
+    getTopRated(@Query(value = "sort_by") String topRated);
 
     @GET(Client.DEFAULT_LIMITED)
-    Observable<ResObj> getLatestUploads(@Query(value = "sort_by") String latestUploads);
+    Observable<ResObj>
+    getLatestUploads(@Query(value = "sort_by") String latestUploads);
 
     @GET(Client.DEFAULT_LIMITED)
-    Observable<ResObj> getThisYear(@Query(value = "sort_by") String thisYear);
+    Observable<ResObj>
+    getThisYear(@Query(value = "sort_by") String thisYear);
 
     @GET(Client.DEFAULT)
-    Observable<ResObj> getNextPage(@Query(value = "sort_by") String sort, @Query(value = "page") int page);
+    Observable<ResObj>
+    getByGenre(@Query(value = "sort_by") String sort, @Query(value = "genre") String genre);
+
+    @GET(Client.DEFAULT)
+    Observable<ResObj>
+    getNextPage(@Query(value = "sort_by") String sort, @Query(value = "genre") String genre, @Query(value = "page") int page);
 
 }
