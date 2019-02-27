463ada33b946fa74bbd239567441e3574e6e7f7e
Jenrielle Gaon <jenriellegaon@icloud.com>
2019-02-27 12:00:00 +0800

    Clear git cache

---
+++ app/src/main/java/com/jproject/ytsmoviebrowser/view/DownloadView.java
@@ -0,0 +1,15 @@
+package com.jproject.ytsmoviebrowser.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jproject.ytsmoviebrowser.R;
+
+public class DownloadView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_download_view);
+    }
+}
